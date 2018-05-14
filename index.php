f88e02a458ed9576986261bd30c551de1b887546
Yousif N.Abbass <yousifnimah5@gmail.com>
2018-05-14 11:42:12 +0300

    RC4 Algorithm Implementation

---
+++ index.php
@@ -0,0 +1,1 @@
+<?phprequire "src/LeonardoDaVinci/Cryptex/Cryptex.php";use Cryptex\Cryptex;$crypt = new Cryptex;$crypt->setAlgorithm('rc4');$cipher = $crypt->encrypt('Hello');$plainText = $crypt->decrypt($cipher);var_dump($cipher);var_dump($plainText);
