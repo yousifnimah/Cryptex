bf3066c48a3c360fbb8bb945cd5e35361f9cdb29
Yousif N.Abbass <yousifnimah5@gmail.com>
2018-05-14 11:44:44 +0300

    Change line separator to LF

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?phprequire "src/LeonardoDaVinci/Cryptex/Cryptex.php";use Cryptex\Cryptex;$crypt = new Cryptex;$crypt->setAlgorithm('rc4');$cipher = $crypt->encrypt('Hello');$plainText = $crypt->decrypt($cipher);var_dump($cipher);var_dump($plainText);
+<?php
+
+require "src/LeonardoDaVinci/Cryptex/Cryptex.php";
+
+use Cryptex\Cryptex;
+
+$crypt = new Cryptex;
+$crypt->setAlgorithm('rc4');
+
+$cipher = $crypt->encrypt('Hello');
+$plainText = $crypt->decrypt($cipher);
+
+
+var_dump($cipher);
+var_dump($plainText);
--- src/LeonardoDaVinci/Cryptex/Algorithm/Algorithm.php
+++ src/LeonardoDaVinci/Cryptex/Algorithm/Algorithm.php
@@ -1,1 +1,108 @@
-<?php namespace Cryptex;use \Exception;class Algorithm{    public $config;    /**     * Algorithm constructor.     */    public function __construct()    {        $this->config = include(dirname(__FILE__) . '/../../config/cryptex.php');    }    /**     * @param $str     * @return string     */    public static function rc4Encryption($str)    {        try {            $instance = new Algorithm();            $config = $instance->config['rc4'];            ///// Algorithm Implementation            $key = $config['key'];            $s = array();            for ($i = 0; $i < 256; $i++) {                $s[$i] = $i;            }            $j = 0;            for ($i = 0; $i < 256; $i++) {                $j = ($j + $s[$i] + ord($key[$i % strlen($key)])) % 256;                $x = $s[$i];                $s[$i] = $s[$j];                $s[$j] = $x;            }            $i = 0;            $j = 0;            $res = '';            for ($y = 0; $y < strlen($str); $y++) {                $i = ($i + 1) % 256;                $j = ($j + $s[$i]) % 256;                $x = $s[$i];                $s[$i] = $s[$j];                $s[$j] = $x;                $res .= $str[$y] ^ chr($s[($s[$i] + $s[$j]) % 256]);            }            return bin2hex($res);            ////// Algorithm Implementation        } catch (Exception $e) {            return "Sorry, there was an error: " . $e->getMessage();        }    }    /**     * @param $str     * @return string     */    public static function rc4Decryption($str)    {        try {            $instance = new Algorithm();            $config = $instance->config['rc4'];            ///// Algorithm Implementation            $key = $config['key'];            $str = hex2bin($str);            $s = array();            for ($i = 0; $i < 256; $i++) {                $s[$i] = $i;            }            $j = 0;            for ($i = 0; $i < 256; $i++) {                $j = ($j + $s[$i] + ord($key[$i % strlen($key)])) % 256;                $x = $s[$i];                $s[$i] = $s[$j];                $s[$j] = $x;            }            $i = 0;            $j = 0;            $res = '';            for ($y = 0; $y < strlen($str); $y++) {                $i = ($i + 1) % 256;                $j = ($j + $s[$i]) % 256;                $x = $s[$i];                $s[$i] = $s[$j];                $s[$j] = $x;                $res .= $str[$y] ^ chr($s[($s[$i] + $s[$j]) % 256]);            }            return $res;            ////// Algorithm Implementation        } catch (Exception $e) {            return "Sorry, there was an error: " . $e->getMessage();        }    }}
+<?php namespace Cryptex;
+
+use \Exception;
+
+
+class Algorithm
+{
+    public $config;
+
+    /**
+     * Algorithm constructor.
+     */
+    public function __construct()
+    {
+        $this->config = include(dirname(__FILE__) . '/../../config/cryptex.php');
+    }
+    /**
+     * @param $str
+     * @return string
+     */
+    public static function rc4Encryption($str)
+    {
+        try {
+            $instance = new Algorithm();
+            $config = $instance->config['rc4'];
+
+            ///// Algorithm Implementation
+
+            $key = $config['key'];
+
+            $s = array();
+            for ($i = 0; $i < 256; $i++) {
+                $s[$i] = $i;
+            }
+            $j = 0;
+            for ($i = 0; $i < 256; $i++) {
+                $j = ($j + $s[$i] + ord($key[$i % strlen($key)])) % 256;
+                $x = $s[$i];
+                $s[$i] = $s[$j];
+                $s[$j] = $x;
+            }
+            $i = 0;
+            $j = 0;
+            $res = '';
+            for ($y = 0; $y < strlen($str); $y++) {
+                $i = ($i + 1) % 256;
+                $j = ($j + $s[$i]) % 256;
+                $x = $s[$i];
+                $s[$i] = $s[$j];
+                $s[$j] = $x;
+                $res .= $str[$y] ^ chr($s[($s[$i] + $s[$j]) % 256]);
+            }
+            return bin2hex($res);
+
+            ////// Algorithm Implementation
+
+        } catch (Exception $e) {
+            return "Sorry, there was an error: " . $e->getMessage();
+        }
+    }
+    /**
+     * @param $str
+     * @return string
+     */
+    public static function rc4Decryption($str)
+    {
+        try {
+            $instance = new Algorithm();
+            $config = $instance->config['rc4'];
+
+            ///// Algorithm Implementation
+
+            $key = $config['key'];
+            $str = hex2bin($str);
+
+            $s = array();
+            for ($i = 0; $i < 256; $i++) {
+                $s[$i] = $i;
+            }
+            $j = 0;
+            for ($i = 0; $i < 256; $i++) {
+                $j = ($j + $s[$i] + ord($key[$i % strlen($key)])) % 256;
+                $x = $s[$i];
+                $s[$i] = $s[$j];
+                $s[$j] = $x;
+            }
+            $i = 0;
+            $j = 0;
+            $res = '';
+            for ($y = 0; $y < strlen($str); $y++) {
+                $i = ($i + 1) % 256;
+                $j = ($j + $s[$i]) % 256;
+                $x = $s[$i];
+                $s[$i] = $s[$j];
+                $s[$j] = $x;
+                $res .= $str[$y] ^ chr($s[($s[$i] + $s[$j]) % 256]);
+            }
+            return $res;
+
+            ////// Algorithm Implementation
+
+        } catch (Exception $e) {
+            return "Sorry, there was an error: " . $e->getMessage();
+        }
+    }
+
+
+}
